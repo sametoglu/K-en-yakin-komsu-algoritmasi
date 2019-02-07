12f394e0360ae3b478afd9d881c3a9561c5b7129
sametoglu <sametoglu-sas@hotmail.com>
2019-02-07 13:00:02 +0300

    K-en yakin komsu algoritmasi Ornegi

---
+++ veri_odevi2/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace veri_odevi2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nud_kdegeri = new System.Windows.Forms.NumericUpDown();
+            this.bt_hesap = new System.Windows.Forms.Button();
+            this.gb_ornekuzay = new System.Windows.Forms.GroupBox();
+            this.nokta = new System.Windows.Forms.Label();
+            this.e1 = new System.Windows.Forms.Label();
+            this.k1 = new System.Windows.Forms.Label();
+            this.k5 = new System.Windows.Forms.Label();
+            this.k6 = new System.Windows.Forms.Label();
+            this.k2 = new System.Windows.Forms.Label();
+            this.e3 = new System.Windows.Forms.Label();
+            this.k0 = new System.Windows.Forms.Label();
+            this.e6 = new System.Windows.Forms.Label();
+            this.k3 = new System.Windows.Forms.Label();
+            this.e7 = new System.Windows.Forms.Label();
+            this.e8 = new System.Windows.Forms.Label();
+            this.k7 = new System.Windows.Forms.Label();
+            this.e0 = new System.Windows.Forms.Label();
+            this.e4 = new System.Windows.Forms.Label();
+            this.e2 = new System.Windows.Forms.Label();
+            this.e5 = new System.Windows.Forms.Label();
+            this.k9 = new System.Windows.Forms.Label();
+            this.e9 = new System.Windows.Forms.Label();
+            this.k8 = new System.Windows.Forms.Label();
+            this.k4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_kdegeri)).BeginInit();
+            this.gb_ornekuzay.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nud_kdegeri
+            // 
+            this.nud_kdegeri.Location = new System.Drawing.Point(518, 12);
+            this.nud_kdegeri.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.nud_kdegeri.Name = "nud_kdegeri";
+            this.nud_kdegeri.Size = new System.Drawing.Size(100, 20);
+            this.nud_kdegeri.TabIndex = 0;
+            // 
+            // bt_hesap
+            // 
+            this.bt_hesap.Location = new System.Drawing.Point(518, 38);
+            this.bt_hesap.Name = "bt_hesap";
+            this.bt_hesap.Size = new System.Drawing.Size(100, 20);
+            this.bt_hesap.TabIndex = 1;
+            this.bt_hesap.Text = "Hesapla";
+            this.bt_hesap.UseVisualStyleBackColor = true;
+            this.bt_hesap.Click += new System.EventHandler(this.bt_hesap_Click);
+            // 
+            // gb_ornekuzay
+            // 
+            this.gb_ornekuzay.Controls.Add(this.nokta);
+            this.gb_ornekuzay.Controls.Add(this.e1);
+            this.gb_ornekuzay.Controls.Add(this.k1);
+            this.gb_ornekuzay.Controls.Add(this.k5);
+            this.gb_ornekuzay.Controls.Add(this.k6);
+            this.gb_ornekuzay.Controls.Add(this.k2);
+            this.gb_ornekuzay.Controls.Add(this.e3);
+            this.gb_ornekuzay.Controls.Add(this.k0);
+            this.gb_ornekuzay.Controls.Add(this.e6);
+            this.gb_ornekuzay.Controls.Add(this.k3);
+            this.gb_ornekuzay.Controls.Add(this.e7);
+            this.gb_ornekuzay.Controls.Add(this.e8);
+            this.gb_ornekuzay.Controls.Add(this.k7);
+            this.gb_ornekuzay.Controls.Add(this.e0);
+            this.gb_ornekuzay.Controls.Add(this.e4);
+            this.gb_ornekuzay.Controls.Add(this.e2);
+            this.gb_ornekuzay.Controls.Add(this.e5);
+            this.gb_ornekuzay.Controls.Add(this.k9);
+            this.gb_ornekuzay.Controls.Add(this.e9);
+            this.gb_ornekuzay.Controls.Add(this.k8);
+            this.gb_ornekuzay.Controls.Add(this.k4);
+            this.gb_ornekuzay.Location = new System.Drawing.Point(12, 12);
+            this.gb_ornekuzay.Name = "gb_ornekuzay";
+            this.gb_ornekuzay.Size = new System.Drawing.Size(500, 500);
+            this.gb_ornekuzay.TabIndex = 3;
+            this.gb_ornekuzay.TabStop = false;
+            this.gb_ornekuzay.Text = "Ornek Uzayı";
+            // 
+            // nokta
+            // 
+            this.nokta.AutoSize = true;
+            this.nokta.Location = new System.Drawing.Point(239, 237);
+            this.nokta.Name = "nokta";
+            this.nokta.Size = new System.Drawing.Size(10, 13);
+            this.nokta.TabIndex = 27;
+            this.nokta.Text = ".";
+            // 
+            // e1
+            // 
+            this.e1.AutoSize = true;
+            this.e1.Location = new System.Drawing.Point(295, 275);
+            this.e1.Name = "e1";
+            this.e1.Size = new System.Drawing.Size(14, 13);
+            this.e1.TabIndex = 31;
+            this.e1.Text = "E";
+            // 
+            // k1
+            // 
+            this.k1.AutoSize = true;
+            this.k1.Location = new System.Drawing.Point(148, 258);
+            this.k1.Name = "k1";
+            this.k1.Size = new System.Drawing.Size(14, 13);
+            this.k1.TabIndex = 30;
+            this.k1.Text = "K";
+            // 
+            // k5
+            // 
+            this.k5.AutoSize = true;
+            this.k5.Location = new System.Drawing.Point(237, 63);
+            this.k5.Name = "k5";
+            this.k5.Size = new System.Drawing.Size(14, 13);
+            this.k5.TabIndex = 29;
+            this.k5.Text = "K";
+            // 
+            // k6
+            // 
+            this.k6.AutoSize = true;
+            this.k6.Location = new System.Drawing.Point(346, 35);
+            this.k6.Name = "k6";
+            this.k6.Size = new System.Drawing.Size(14, 13);
+            this.k6.TabIndex = 28;
+            this.k6.Text = "K";
+            // 
+            // k2
+            // 
+            this.k2.AutoSize = true;
+            this.k2.Location = new System.Drawing.Point(203, 237);
+            this.k2.Name = "k2";
+            this.k2.Size = new System.Drawing.Size(14, 13);
+            this.k2.TabIndex = 27;
+            this.k2.Text = "K";
+            // 
+            // e3
+            // 
+            this.e3.AutoSize = true;
+            this.e3.Location = new System.Drawing.Point(235, 415);
+            this.e3.Name = "e3";
+            this.e3.Size = new System.Drawing.Size(14, 13);
+            this.e3.TabIndex = 26;
+            this.e3.Text = "E";
+            // 
+            // k0
+            // 
+            this.k0.AutoSize = true;
+            this.k0.Location = new System.Drawing.Point(237, 35);
+            this.k0.Name = "k0";
+            this.k0.Size = new System.Drawing.Size(14, 13);
+            this.k0.TabIndex = 24;
+            this.k0.Text = "K";
+            // 
+            // e6
+            // 
+            this.e6.AutoSize = true;
+            this.e6.Location = new System.Drawing.Point(480, 475);
+            this.e6.Name = "e6";
+            this.e6.Size = new System.Drawing.Size(14, 13);
+            this.e6.TabIndex = 23;
+            this.e6.Text = "E";
+            // 
+            // k3
+            // 
+            this.k3.AutoSize = true;
+            this.k3.Location = new System.Drawing.Point(148, 221);
+            this.k3.Name = "k3";
+            this.k3.Size = new System.Drawing.Size(14, 13);
+            this.k3.TabIndex = 21;
+            this.k3.Text = "K";
+            // 
+            // e7
+            // 
+            this.e7.AutoSize = true;
+            this.e7.Location = new System.Drawing.Point(393, 275);
+            this.e7.Name = "e7";
+            this.e7.Size = new System.Drawing.Size(14, 13);
+            this.e7.TabIndex = 16;
+            this.e7.Text = "E";
+            // 
+            // e8
+            // 
+            this.e8.AutoSize = true;
+            this.e8.Location = new System.Drawing.Point(295, 237);
+            this.e8.Name = "e8";
+            this.e8.Size = new System.Drawing.Size(14, 13);
+            this.e8.TabIndex = 14;
+            this.e8.Text = "E";
+            // 
+            // k7
+            // 
+            this.k7.AutoSize = true;
+            this.k7.Location = new System.Drawing.Point(346, 7);
+            this.k7.Name = "k7";
+            this.k7.Size = new System.Drawing.Size(14, 13);
+            this.k7.TabIndex = 12;
+            this.k7.Text = "K";
+            // 
+            // e0
+            // 
+            this.e0.AutoSize = true;
+            this.e0.Location = new System.Drawing.Point(393, 196);
+            this.e0.Name = "e0";
+            this.e0.Size = new System.Drawing.Size(14, 13);
+            this.e0.TabIndex = 11;
+            this.e0.Text = "E";
+            // 
+            // e4
+            // 
+            this.e4.AutoSize = true;
+            this.e4.Location = new System.Drawing.Point(480, 445);
+            this.e4.Name = "e4";
+            this.e4.Size = new System.Drawing.Size(14, 13);
+            this.e4.TabIndex = 8;
+            this.e4.Text = "E";
+            // 
+            // e2
+            // 
+            this.e2.AutoSize = true;
+            this.e2.Location = new System.Drawing.Point(480, 415);
+            this.e2.Name = "e2";
+            this.e2.Size = new System.Drawing.Size(14, 13);
+            this.e2.TabIndex = 7;
+            this.e2.Text = "E";
+            // 
+            // e5
+            // 
+            this.e5.AutoSize = true;
+            this.e5.Location = new System.Drawing.Point(393, 237);
+            this.e5.Name = "e5";
+            this.e5.Size = new System.Drawing.Size(14, 13);
+            this.e5.TabIndex = 6;
+            this.e5.Text = "E";
+            // 
+            // k9
+            // 
+            this.k9.AutoSize = true;
+            this.k9.Location = new System.Drawing.Point(186, 35);
+            this.k9.Name = "k9";
+            this.k9.Size = new System.Drawing.Size(14, 13);
+            this.k9.TabIndex = 4;
+            this.k9.Text = "K";
+            // 
+            // e9
+            // 
+            this.e9.AutoSize = true;
+            this.e9.Location = new System.Drawing.Point(295, 196);
+            this.e9.Name = "e9";
+            this.e9.Size = new System.Drawing.Size(14, 13);
+            this.e9.TabIndex = 3;
+            this.e9.Text = "E";
+            // 
+            // k8
+            // 
+            this.k8.AutoSize = true;
+            this.k8.Location = new System.Drawing.Point(111, 210);
+            this.k8.Name = "k8";
+            this.k8.Size = new System.Drawing.Size(14, 13);
+            this.k8.TabIndex = 2;
+            this.k8.Text = "K";
+            // 
+            // k4
+            // 
+            this.k4.AutoSize = true;
+            this.k4.Location = new System.Drawing.Point(111, 237);
+            this.k4.Name = "k4";
+            this.k4.Size = new System.Drawing.Size(14, 13);
+            this.k4.TabIndex = 1;
+            this.k4.Text = "K";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 524);
+            this.Controls.Add(this.gb_ornekuzay);
+            this.Controls.Add(this.bt_hesap);
+            this.Controls.Add(this.nud_kdegeri);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nud_kdegeri)).EndInit();
+            this.gb_ornekuzay.ResumeLayout(false);
+            this.gb_ornekuzay.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nud_kdegeri;
+        private System.Windows.Forms.Button bt_hesap;
+        private System.Windows.Forms.GroupBox gb_ornekuzay;
+        private System.Windows.Forms.Label k1;
+        private System.Windows.Forms.Label k5;
+        private System.Windows.Forms.Label k6;
+        private System.Windows.Forms.Label k2;
+        private System.Windows.Forms.Label e3;
+        private System.Windows.Forms.Label e6;
+        private System.Windows.Forms.Label k3;
+        private System.Windows.Forms.Label e7;
+        private System.Windows.Forms.Label e8;
+        private System.Windows.Forms.Label k7;
+        private System.Windows.Forms.Label e0;
+        private System.Windows.Forms.Label e4;
+        private System.Windows.Forms.Label e2;
+        private System.Windows.Forms.Label e5;
+        private System.Windows.Forms.Label k9;
+        private System.Windows.Forms.Label e9;
+        private System.Windows.Forms.Label k8;
+        private System.Windows.Forms.Label k4;
+        private System.Windows.Forms.Label e1;
+        private System.Windows.Forms.Label k0;
+        private System.Windows.Forms.Label nokta;
+
+    }
+}
+
---
+++ veri_odevi2/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace veri_odevi2
+{
+    public partial class Form1 : Form
+    {
+        double[] E = new double[10];
+        double[] K = new double[10];
+
+
+
+        int k;
+
+        public void sıralıatama(double[] dizi) {
+
+            for (int i = 0; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    if (dizi[j - 1] > dizi[j])
+                    {
+                        double gecici = dizi[j - 1];
+                        dizi[j - 1] = dizi[j];
+                        dizi[j] = gecici;
+                    }
+                }
+            }
+
+        
+        }
+
+        public void diziyazdir(double[] dizi)
+        {
+
+            for (int i = 0; i <= 9; i++)
+            {
+                MessageBox.Show(dizi[i].ToString());
+            }
+
+        }
+
+        public void hesap(int k)
+        {
+
+            double ue = 0;
+            double uk = 0;
+
+            
+
+
+            for (int i = 0; i < k; i++)
+            {
+
+                
+
+                ue += E[i];
+                uk += K[i];
+
+                
+
+            }
+                ue = ue / k;
+                uk = uk / k;
+               
+
+                if (ue > uk)
+                {
+                    MessageBox.Show("K sınıfına yakın");
+                    
+                }
+
+                else
+                {
+                    MessageBox.Show("E sınıfına yakın");
+                   
+                }
+
+
+
+            
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*
+             *
+             * noktanın x ve y si;
+             * k degeri;
+             * label1x,label1y hepsi icin;
+             * sqrt((label1x)*(label1x)+(label1y)*(label1y))=u1 hepsi icin;
+             * yukarıda hesapladıklarımızı kucukten buyuge sıralı halde diziye atarız.
+             * k ' ye gore dizi ' nin elemanları ortalaması alınır ve sonuc. 
+             * 
+             */
+
+            
+
+            int noktax = nokta.Location.X;
+            int noktay = nokta.Location.Y;
+
+            int e0x = e0.Location.X;
+            int e1x = e1.Location.X;
+            int e2x = e2.Location.X;
+            int e3x = e3.Location.X;
+            int e4x = e4.Location.X;
+            int e5x = e5.Location.X;
+            int e6x = e6.Location.X;
+            int e7x = e7.Location.X;
+            int e8x = e8.Location.X;
+            int e9x = e9.Location.X;
+
+            
+
+            int e0y = e0.Location.Y;
+            int e1y = e1.Location.Y;
+            int e2y = e2.Location.Y;
+            int e3y = e3.Location.Y;
+            int e4y = e4.Location.Y;
+            int e5y = e5.Location.Y;
+            int e6y = e6.Location.Y;
+            int e7y = e7.Location.Y;
+            int e8y = e8.Location.Y;
+            int e9y = e9.Location.Y;
+
+            
+
+
+            int k0x = k0.Location.X;
+            int k1x = k1.Location.X;
+            int k2x = k2.Location.X;
+            int k3x = k3.Location.X;
+            int k4x = k4.Location.X;
+            int k5x = k5.Location.X;
+            int k6x = k6.Location.X;
+            int k7x = k7.Location.X;
+            int k8x = k8.Location.X;
+            int k9x = k9.Location.X;
+
+            
+
+            int k0y = k0.Location.Y;
+            int k1y = k1.Location.Y;
+            int k2y = k2.Location.Y;
+            int k3y = k3.Location.Y;
+            int k4y = k4.Location.Y;
+            int k5y = k5.Location.Y;
+            int k6y = k6.Location.Y;
+            int k7y = k7.Location.Y;
+            int k8y = k8.Location.Y;
+            int k9y = k9.Location.Y;
+
+           
+            E[1] = Math.Sqrt((Math.Abs(e1x - noktax) * Math.Abs(e1x - noktax)) + (Math.Abs(e1y - noktay) * Math.Abs(e1y - noktay)));
+            E[2] = Math.Sqrt((Math.Abs(e2x - noktax) * Math.Abs(e2x - noktax)) + (Math.Abs(e2y - noktay) * Math.Abs(e2y - noktay)));
+            E[3] = Math.Sqrt((Math.Abs(e3x - noktax) * Math.Abs(e3x - noktax)) + (Math.Abs(e3y - noktay) * Math.Abs(e3y - noktay)));
+            E[4] = Math.Sqrt((Math.Abs(e4x - noktax) * Math.Abs(e4x - noktax)) + (Math.Abs(e4y - noktay) * Math.Abs(e4y - noktay)));
+            E[5] = Math.Sqrt((Math.Abs(e5x - noktax) * Math.Abs(e5x - noktax)) + (Math.Abs(e5y - noktay) * Math.Abs(e5y - noktay)));
+            E[6] = Math.Sqrt((Math.Abs(e6x - noktax) * Math.Abs(e6x - noktax)) + (Math.Abs(e6y - noktay) * Math.Abs(e6y - noktay)));
+            E[7] = Math.Sqrt((Math.Abs(e7x - noktax) * Math.Abs(e7x - noktax)) + (Math.Abs(e7y - noktay) * Math.Abs(e7y - noktay)));
+            E[8] = Math.Sqrt((Math.Abs(e8x - noktax) * Math.Abs(e8x - noktax)) + (Math.Abs(e8y - noktay) * Math.Abs(e8y - noktay)));
+            E[9] = Math.Sqrt((Math.Abs(e9x - noktax) * Math.Abs(e9x - noktax)) + (Math.Abs(e9y - noktay) * Math.Abs(e9y - noktay)));
+            E[0] = Math.Sqrt((Math.Abs(e0x - noktax) * Math.Abs(e0x - noktax)) + (Math.Abs(e0y - noktay) * Math.Abs(e0y - noktay)));
+
+            sıralıatama(E);
+            
+
+            K[1] = Math.Sqrt((Math.Abs(k1x - noktax) * Math.Abs(k1x - noktax)) + (Math.Abs(k1y - noktay) * Math.Abs(k1y - noktay)));
+            K[2] = Math.Sqrt((Math.Abs(k2x - noktax) * Math.Abs(k2x - noktax)) + (Math.Abs(k2y - noktay) * Math.Abs(k2y - noktay)));
+            K[3] = Math.Sqrt((Math.Abs(k3x - noktax) * Math.Abs(k3x - noktax)) + (Math.Abs(k3y - noktay) * Math.Abs(k3y - noktay)));
+            K[4] = Math.Sqrt((Math.Abs(k4x - noktax) * Math.Abs(k4x - noktax)) + (Math.Abs(k4y - noktay) * Math.Abs(k4y - noktay)));
+            K[5] = Math.Sqrt((Math.Abs(k5x - noktax) * Math.Abs(k5x - noktax)) + (Math.Abs(k5y - noktay) * Math.Abs(k5y - noktay)));
+            K[6] = Math.Sqrt((Math.Abs(k6x - noktax) * Math.Abs(k6x - noktax)) + (Math.Abs(k6y - noktay) * Math.Abs(k6y - noktay)));
+            K[7] = Math.Sqrt((Math.Abs(k7x - noktax) * Math.Abs(k7x - noktax)) + (Math.Abs(k7y - noktay) * Math.Abs(k7y - noktay)));
+            K[8] = Math.Sqrt((Math.Abs(k8x - noktax) * Math.Abs(k8x - noktax)) + (Math.Abs(k8y - noktay) * Math.Abs(k8y - noktay)));
+            K[9] = Math.Sqrt((Math.Abs(k9x - noktax) * Math.Abs(k9x - noktax)) + (Math.Abs(k9y - noktay) * Math.Abs(k9y - noktay)));
+            K[0] = Math.Sqrt((Math.Abs(k0x - noktax) * Math.Abs(k0x - noktax)) + (Math.Abs(k0y - noktay) * Math.Abs(k0y - noktay)));
+
+            sıralıatama(K);
+
+        }
+
+        private void bt_hesap_Click(object sender, EventArgs e)
+        {
+            //diziyazdir(E);
+            //diziyazdir(K);
+            hesap(Convert.ToInt32(nud_kdegeri.Value));
+        }
+
+        
+    }
+}
